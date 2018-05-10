359acc68eb59fee7b7be5a369cda5d4cc91fb564
meowoodie <woodielove@163.com>
2018-05-10 13:34:13 -0400

    minor update

--- run.py
+++ run.py
@@ -28,49 +28,46 @@ if __name__ == "__main__":
     # get corpus matrix
     data_x = corpus2dense(corpus_tfidf, num_terms=len(ngram_dict)).transpose()
     n_x    = data_x.shape[1]
+
     print(data_x.shape)
 
-    # get labels info
-    labels = None
-    with open(info_name, "r") as f:
-        # fetch labels
-        labels = np.array([ line.strip().split("\t")[1] for line in f ])
-    label_set = list(set(labels))
-    n_y       = len(set(labels))
-    data_y    = []
-    for label in labels:
-        d = np.zeros(n_y)
-        d[label_set.index(label)] = 1.
-        data_y.append(d)
-    data_y    = np.array(data_y)
+    t   = 1e-2
+    lam = 1e-3
+    lr  = 1e-3
+    n_epoches = 25
 
-    print(data_y.shape)
+    rbm = RegRBM(n_visible=n_x, n_hidden=1000, t=t, lam=lam, \
+                 learning_rate=lr, momentum=0.95, err_function="mse", \
+                 sample_visible=False, sigma=1.)
+    errs, zeros = rbm.fit(data_x, n_epoches=n_epoches, batch_size=20, \
+                          shuffle=True, verbose=True)
+
+    embeddings = rbm.transform(data_x).round().astype(int)
+
+    # save results
+    np.savetxt("resource/errors.lam%1.e.lr%1.e.t%1.e.epoch%d.txt" % (lam, lr, t, n_epoches), errs, delimiter=',')
+    np.savetxt("resource/zeros.lam%1.e.lr%1.e.t%1.e.epoch%d.txt" % (lam, lr, t, n_epoches), zeros, delimiter=',')
+    # np.savetxt("resource/embeddings/2k.embeddings.reg.1e-3.lr.1e-3.txt", embeddings, delimiter=',')
+    # vec2tsne(info_name, "results/test.pdf", vectors=embeddings, n=2)
+
+    # # get labels info
+    # labels = None
+    # with open(info_name, "r") as f:
+    #     # fetch labels
+    #     labels = np.array([ line.strip().split("\t")[1] for line in f ])
+    # label_set = list(set(labels))
+    # n_y       = len(set(labels))
+    # data_y    = []
+    # for label in labels:
+    #     d = np.zeros(n_y)
+    #     d[label_set.index(label)] = 1.
+    #     data_y.append(d)
+    # data_y    = np.array(data_y)
+    #
+    # print(data_y.shape)
 
     # rbm = SemiSupervRBM(n_y=n_y, n_x=n_x, n_h=1000, alpha=.5, batch_size=20, \
     #                     learning_rate=.01, momentum=0.95, err_function='mse', \
     #                     sample_visible=False)
     # rbm.fit(data_x, data_y, n_epoches=100, shuffle=True)
     # embeddings = rbm.transform(data_x).round().astype(int)
-
-    rbm = RegRBM(n_visible=n_x, n_hidden=1000, \
-                learning_rate=1e-3, momentum=0.95, err_function='mse', \
-                use_tqdm=False, sample_visible=False, sigma=1.)
-    rbm.fit(data_x, n_epoches=20, batch_size=20, \
-            shuffle=True, verbose=True)
-
-    # rbm = GBRBM(n_visible=n_x, n_hidden=1000, \
-    #             learning_rate=1e-3, momentum=0.95, err_function='mse', \
-    #             use_tqdm=False, sample_visible=False, sigma=1.)
-    # rbm.fit(data_x, n_epoches=10, batch_size=20, \
-    #         shuffle=True, verbose=True)
-    embeddings = rbm.transform(data_x).round().astype(int)
-    recon_x    = rbm.reconstruct(data_x)
-    print(recon_x.shape)
-
-    recon_x[recon_x < 0] = 0
-    # mat2img(-1 * np.log(recon_x))
-
-    # # save embeddings
-    file_name="reg.2k.recon"
-    np.savetxt("resource/embeddings/%s.txt" % file_name, recon_x, delimiter=',')
-    # vec2tsne(info_name, "results/test.pdf", vectors=embeddings, n=2)
