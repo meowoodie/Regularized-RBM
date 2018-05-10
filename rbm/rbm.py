2fe3821477834d3c12f2d6b002275c6fa383e479
meowoodie <woodielove@163.com>
2018-05-10 13:34:00 -0400

    add get zeros

--- rbm/rbm.py
+++ rbm/rbm.py
@@ -13,42 +13,32 @@ class RBM:
                  learning_rate=0.01,
                  momentum=0.95,
                  xavier_const=1.0,
-                 err_function='mse',
-                 use_tqdm=False,
-                 # DEPRECATED:
-                 tqdm=None):
+                 err_function="mse"):
         if not 0.0 <= momentum <= 1.0:
-            raise ValueError('momentum should be in range [0, 1]')
+            raise ValueError("momentum should be in range [0, 1]")
 
-        if err_function not in {'mse', 'cosine'}:
-            raise ValueError('err_function should be either \'mse\' or \'cosine\'')
+        if err_function not in {"mse", "cosine"}:
+            raise ValueError("err_function should be either \"mse\" or \"cosine\"")
 
-        self._use_tqdm = use_tqdm
-        self._tqdm = None
-
-        if use_tqdm or tqdm is not None:
-            from tqdm import tqdm
-            self._tqdm = tqdm
-
-        self.n_visible = n_visible
-        self.n_hidden = n_hidden
+        self.n_visible     = n_visible
+        self.n_hidden      = n_hidden
         self.learning_rate = learning_rate
-        self.momentum = momentum
+        self.momentum      = momentum
 
         self.x = tf.placeholder(tf.float32, [None, self.n_visible])
         self.y = tf.placeholder(tf.float32, [None, self.n_hidden])
 
-        self.w = tf.Variable(tf_xavier_init(self.n_visible, self.n_hidden, const=xavier_const), dtype=tf.float32)
+        self.w            = tf.Variable(tf_xavier_init(self.n_visible, self.n_hidden, const=xavier_const), dtype=tf.float32)
         self.visible_bias = tf.Variable(tf.zeros([self.n_visible]), dtype=tf.float32)
-        self.hidden_bias = tf.Variable(tf.zeros([self.n_hidden]), dtype=tf.float32)
+        self.hidden_bias  = tf.Variable(tf.zeros([self.n_hidden]), dtype=tf.float32)
 
-        self.delta_w = tf.Variable(tf.zeros([self.n_visible, self.n_hidden]), dtype=tf.float32)
+        self.delta_w            = tf.Variable(tf.zeros([self.n_visible, self.n_hidden]), dtype=tf.float32)
         self.delta_visible_bias = tf.Variable(tf.zeros([self.n_visible]), dtype=tf.float32)
-        self.delta_hidden_bias = tf.Variable(tf.zeros([self.n_hidden]), dtype=tf.float32)
+        self.delta_hidden_bias  = tf.Variable(tf.zeros([self.n_hidden]), dtype=tf.float32)
 
-        self.update_weights = None
-        self.update_deltas = None
-        self.compute_hidden = None
+        self.update_weights  = None
+        self.update_deltas   = None
+        self.compute_hidden  = None
         self.compute_visible = None
         self.compute_visible_from_hidden = None
 
@@ -60,7 +50,7 @@ class RBM:
         assert self.compute_visible is not None
         assert self.compute_visible_from_hidden is not None
 
-        if err_function == 'cosine':
+        if err_function == "cosine":
             x1_norm = tf.nn.l2_normalize(self.x, 1)
             x2_norm = tf.nn.l2_normalize(self.compute_visible, 1)
             cos_val = tf.reduce_mean(tf.reduce_sum(tf.mul(x1_norm, x2_norm), 1))
@@ -74,6 +64,15 @@ class RBM:
 
     def _initialize_vars(self):
         pass
+
+    def get_zero(self, batch_x):
+        """
+        return zero variables of reconstructed x.
+        """
+        recon_x = self.reconstruct(batch_x)
+        recon_x[recon_x < self.t] = 0
+        n_zro_vars = len(np.where(~recon_x.any(axis=0))[0])
+        return n_zro_vars
 
     def get_err(self, batch_x):
         return self.sess.run(self.compute_err, feed_dict={self.x: batch_x})
@@ -115,46 +114,38 @@ class RBM:
             data_x_cpy = data_x
 
         errs = []
+        zros = []
 
         for e in range(n_epoches):
-            if verbose and not self._use_tqdm:
-                print('Epoch: {:d}'.format(e))
 
             epoch_errs = np.zeros((n_batches,))
-            epoch_errs_ptr = 0
+            epoch_zros = np.zeros((n_batches,))
+            epoch_ptr  = 0
 
             if shuffle:
                 np.random.shuffle(inds)
                 data_x_cpy = data_x_cpy[inds]
 
-            r_batches = range(n_batches)
-
-            if verbose and self._use_tqdm:
-                r_batches = self._tqdm(r_batches, desc='Epoch: {:d}'.format(e), ascii=True, file=sys.stdout)
-
-            for b in r_batches:
+            for b in range(n_batches):
                 batch_x = data_x_cpy[b * batch_size:(b + 1) * batch_size]
                 self.partial_fit(batch_x)
                 batch_err = self.get_err(batch_x)
-                epoch_errs[epoch_errs_ptr] = batch_err
-                epoch_errs_ptr += 1
-
-            recon_y = self.sess.run(self.compute_visible, feed_dict={self.x: batch_x})
-            print("total %d, zero %d" % (recon_y.size, len(recon_y[np.where(recon_y <= self.t)])))
+                batch_zro = self.get_zero(batch_x)
+                epoch_errs[epoch_ptr] = batch_err
+                epoch_zros[epoch_ptr] = batch_zro
+                epoch_ptr += 1
 
             if verbose:
                 err_mean = epoch_errs.mean()
-                if self._use_tqdm:
-                    self._tqdm.write('Train error: {:.4f}'.format(err_mean))
-                    self._tqdm.write('')
-                else:
-                    print('Train error: {:.4f}'.format(err_mean))
-                    print('')
-                sys.stdout.flush()
+                zro_mean = epoch_zros.mean()
+                print("Epoch: {:d}".format(e))
+                print("Train error: {:.4f}".format(err_mean))
+                print("total %d, zero %d\n" % (self.n_visible, zro_mean))
 
             errs = np.hstack([errs, epoch_errs])
+            zros = np.hstack([zros, epoch_zros])
 
-        return errs
+        return errs, zeros
 
     def get_weights(self):
         return self.sess.run(self.w),\
@@ -162,9 +153,9 @@ class RBM:
             self.sess.run(self.hidden_bias)
 
     def save_weights(self, filename, name):
-        saver = tf.train.Saver({name + '_w': self.w,
-                                name + '_v': self.visible_bias,
-                                name + '_h': self.hidden_bias})
+        saver = tf.train.Saver({name + "_w": self.w,
+                                name + "_v": self.visible_bias,
+                                name + "_h": self.hidden_bias})
         return saver.save(self.sess, filename)
 
     def set_weights(self, w, visible_bias, hidden_bias):
@@ -173,7 +164,7 @@ class RBM:
         self.sess.run(self.hidden_bias.assign(hidden_bias))
 
     def load_weights(self, filename, name):
-        saver = tf.train.Saver({name + '_w': self.w,
-                                name + '_v': self.visible_bias,
-                                name + '_h': self.hidden_bias})
+        saver = tf.train.Saver({name + "_w": self.w,
+                                name + "_v": self.visible_bias,
+                                name + "_h": self.hidden_bias})
         saver.restore(self.sess, filename)
